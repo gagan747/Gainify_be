9a143f679f3ee57d4de01c6e85a2106ce86c5eec
gurjot74 <gagan.singh@tothenew.com>
2024-08-11 16:31:28 +0530

    fixed cors error

--- index.js
+++ index.js
@@ -16,7 +16,12 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json()); // Middleware to parse JSON bodies
 
-app.use(cors());
+app.use(cors({
+ origin: '*',
+ methods: ['GET', 'POST', 'PUT', 'DELETE'],
+ allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
 
 app.use('/server-health',(req,res, next)=>{
  res.status(200).json({ message: 'Server is healthy...' });
